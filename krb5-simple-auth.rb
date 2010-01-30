df9eecfd8de0c130d40422d6591320afe393f771
Tim Felgentreff <timfelgentreff@gmail.com>
2010-01-30 22:46:57 +0100

    add a ruby ffi module

---
+++ krb5-simple-auth.rb
@@ -0,0 +1,11 @@
+require 'ffi'
+module SimpleKrb5
+  extend FFI::Library
+   
+  ffi_lib "krb5_simple_auth.so"
+  attach_function(:Krb5_get_init_creds_password, [:string, :string], :int)
+
+  def authenticate(user, password)
+    Krb5_get_init_creds_password(user, password) == 1
+  end
+end
